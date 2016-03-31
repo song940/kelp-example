5548f218c5a9ea50b5ab6109bf2679ecc6c62cab
Lsong <song940@gmail.com>
2016-03-31 23:16:50 +0800

    add kelp-captcha

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 'use strict';
-const http   = require('http');
-const kelp   = require('kelp');
-const auth   = require('kelp-auth');
-const body   = require('kelp-body');
-const send   = require('kelp-send');
-const error  = require('kelp-error');
-const route  = require('kelp-route');
-const serve  = require('kelp-static');
-const logger = require('kelp-logger');
-const config = require('kelp-config');
-const render = require('kelp-render');
-const cookie = require('kelp-cookie');
+const http    = require('http');
+const kelp    = require('kelp');
+const auth    = require('kelp-auth');
+const body    = require('kelp-body');
+const send    = require('kelp-send');
+const error   = require('kelp-error');
+const route   = require('kelp-route');
+const serve   = require('kelp-static');
+const logger  = require('kelp-logger');
+const config  = require('kelp-config');
+const render  = require('kelp-render');
+const cookie  = require('kelp-cookie');
+const captcha = require('kelp-captcha');
 
 const app = kelp()
 
@@ -21,11 +22,14 @@ const app = kelp()
 .use(cookie)
 .use(serve(config.public))
 
-// route, auth and cookie example
-app.use(route('/admin', auth("user", "pass", 'Hello!'), function(req, res){
-  res.cookie('name', 'test', { path: '/' });
-  res.send('admin', req.cookies);
-}));
+app.use(route('/captcha.jpg', captcha({
+  width     : 250   ,
+  height    : 100   ,
+  length    : 6     ,
+  color     : '#fff',
+  fontSize  : 60    ,
+  background: 'rgb(20,30,200)'
+})));
 
 // default handler
 app.use(function(req, res){
